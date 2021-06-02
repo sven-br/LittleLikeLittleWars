d91d38c34931539a507672f3b997602154b4e802
Laurenz <laurenz.grueter@byterockers.games>
2021-06-02 16:13:03 +0200

    added colours

--- Assets/Scripts/Star.cs
+++ Assets/Scripts/Star.cs
@@ -9,6 +9,15 @@ public class Star : MonoBehaviour, IMessageReceiver, IUnitTransferable
     [SerializeField] private Owner owner = Owner.neutral;
     [SerializeField] private GameObject selectedOutline = null;
 
+    Dictionary<Owner, Color> colormapping = new Dictionary<Owner, Color>()
+{
+    { Owner.player0, Color.red },
+    { Owner.player1, Color.yellow },
+    { Owner.player2, Color.blue },
+    { Owner.player3, Color.green },
+    { Owner.neutral, Color.grey }
+};
+
     public enum Owner
     {
         player0,
@@ -18,10 +27,22 @@ public class Star : MonoBehaviour, IMessageReceiver, IUnitTransferable
         neutral,
     }
 
+    private void SetOwner(Owner owner)
+    {
+        this.owner = owner;
+        SetColour();
+    }
+
     public int Units
     {
         get { return units; }
         private set { units = value; UpdateText(); }
+    }
+
+    private void SetColour()
+    {
+        SpriteRenderer renderer = GetComponent<SpriteRenderer>();
+        renderer.color = colormapping[owner];
     }
 
     void Start()
@@ -31,6 +52,7 @@ public class Star : MonoBehaviour, IMessageReceiver, IUnitTransferable
         MessageManager.StartReceivingMessage<StarSelectedMessage>(this);
         MessageManager.StartReceivingMessage<AllStarsUnselectedMessage>(this);
         MessageManager.StartReceivingMessage<TickMessage>(this);
+        SetColour();
     }
 
     void Update()
