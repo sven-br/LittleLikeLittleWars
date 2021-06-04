1d1adbe484e422715c5bf21ff1a9d5d99ade9c9e
Laurenz-BR <laurenz.grueter@byterockers.games>
2021-06-04 11:20:42 +0200

    added unstaged files again ._.

---
+++ Assets/Scripts/MessageSystem/Messages/SpaceFightMessage.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceFightMessage : Message
+{
+	public int amount;
+	public ObjectOwner owner;
+	public Unit opponent;
+}
