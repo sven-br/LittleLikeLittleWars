1fb9ed2d17f104c2f0ca297763d61332d199be28
sven-br <sven.erdem@byterockers.games>
2021-06-08 13:16:34 +0200

    swap player colors

--- Assets/Scripts/Colourmapping.cs
+++ Assets/Scripts/Colourmapping.cs
@@ -7,8 +7,8 @@ public static class ColourMapping
 
     public static Dictionary<ObjectOwner, Color> colormapping = new Dictionary<ObjectOwner, Color>()
     {
-        { ObjectOwner.player0, new Color(184 / 255.0f, 78 / 255.0f, 20 / 255.0f) },
-        { ObjectOwner.player1, new Color(15 / 255.0f, 78 / 255.0f, 180 / 255.0f) },
+        { ObjectOwner.player0, new Color(15 / 255.0f, 78 / 255.0f, 180 / 255.0f) },
+        { ObjectOwner.player1, new Color(184 / 255.0f, 78 / 255.0f, 20 / 255.0f) },
         { ObjectOwner.player2, Color.yellow },
         { ObjectOwner.player3, Color.green },
         { ObjectOwner.neutral, new Color(99 / 255.0f, 98 / 255.0f, 64 / 255.0f) }
